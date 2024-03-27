2e8d3260b1627ef1fd0aa08021bc64e5d8cd9192
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-03-27 10:17:34 +0300

    refactoring databse connection

--- api/server.js
+++ api/server.js
@@ -1,9 +1,20 @@
 import express from 'express'
-
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+dotenv.config()
 
 const app = express();
+const PORT = process.env.PORT
 
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to the database')
+}).catch((err) => {
+    console.error('Error connecting to the database:', err)
+})
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running')
 });
+
+
+
