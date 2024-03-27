4fe51fa62fe943732fbf74fa617cc6dc9ee29d49
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-03-27 11:36:54 +0300

    signin api route functionality completed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js"
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 
 
 
@@ -17,6 +20,28 @@ export const signup = async (req, res, next) => {
         res
         .status(201)
         .json({message: 'Account created Successfuly'});
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const signin = async (req, res, next) => {
+    try {
+        const {email, password} = req.body;
+        //check if email exists
+        const validUser = await User.findOne({email});
+        if (!validUser) return next(errorHandler(400, 'Acconut does not exist'))
+        //verify password
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) return next(errorHandler(404,'Incorrect email or password'))
+        
+        const token = jwt.sign({id: validUser._id, isAdmin: validUser.isAdmin}, process.env.JWT_KEY);
+        const {password: pass, ...rest} = validUser._doc
+
+        res
+        .cookie('access_token', token, {httpOnly: true})
+        .status(200)
+        .json(rest)
     } catch (error) {
         next(error)
     }
